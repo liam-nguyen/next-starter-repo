7fe1434ec2f420d82a9b43bcf5d34458d1f5e34c
Liam Nguyen <nguyen.dch@gmail.com>
2025-11-16 00:11:56 -0800

    initial commit

---
+++ eslint.config.cjs
@@ -0,0 +1,41 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "prefer-const": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "*.lock",
+      "pnpm-lock.yaml",
+      "package-lock.json",
+      "yarn.lock",
+      ".DS_Store",
+      "*.log",
+      ".env*.local",
+      ".vercel/**",
+      ".turbo/**",
+    ],
+  },
+];
+
